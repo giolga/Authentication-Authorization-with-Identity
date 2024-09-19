4cbaed799f89c7b25736476d763fe21dc655a5ae
giolga <giolga.kumi@gmail.com>
2024-09-19 15:02:12 +0400

    authentication added, some features added

--- BlazorAppIdentityDotNet8/BlazorAppIdentityDotNet8.Client/obj/Debug/net8.0/BlazorAppIdentityDotNet8.Client.AssemblyInfo.cs
+++ BlazorAppIdentityDotNet8/BlazorAppIdentityDotNet8.Client/obj/Debug/net8.0/BlazorAppIdentityDotNet8.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppIdentityDotNet8.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7238b65f1b233be189772ebd9fd6eeddbe4897c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppIdentityDotNet8.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppIdentityDotNet8.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
